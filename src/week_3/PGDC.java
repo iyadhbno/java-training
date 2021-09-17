b130b5ff7791d4beb5accbab489690d9ebdbf576
iyadhbno <58296747+iyadhbno@users.noreply.github.com>
2021-09-17 01:25:19 +0100

    fix: PGCD

--- src/week_3/PGDC.java
+++ src/week_3/PGDC.java
@@ -10,17 +10,24 @@ public class PGDC {
 		int nb1 = scanner.nextInt();
 		System.out.print("Entrez un nombre positif : ");
 		int nb2 = scanner.nextInt();
-
-		int a = nb1;
-		int b = nb2;
-		while (a != b) {
-			if (a > b) {
-				a = a - b;
-			} else {
-				b = b - a;
+		if (nb1 == 0 && nb2 == 0) {
+			System.out.println("Le plus grand diviseur commun de " + nb1 + " et " + nb2 + " est " + nb1);
+		} else if (nb1 == 0) {
+			System.out.println("Le plus grand diviseur commun de " + nb1 + " et " + nb2 + " est " + nb2);
+		} else if (nb2 == 0) {
+			System.out.println("Le plus grand diviseur commun de " + nb1 + " et " + nb2 + " est " + nb1);
+		} else {
+			int a = nb1;
+			int b = nb2;
+			while (a != b) {
+				if (a > b) {
+					a = a - b;
+				} else {
+					b = b - a;
+				}
 			}
+			System.out.println("Le plus grand diviseur commun de " + nb1 + " et " + nb2 + " est " + a);
+			scanner.close();
 		}
-		System.out.println("Le plus grand diviseur commun de " + nb1 + " et " + nb2 + " est " + a);
-		scanner.close();
 	}
 }
