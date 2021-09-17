05558e4f55e62d6ac5a6ebae6d1b671d9ab5b728
iyadhbno <58296747+iyadhbno@users.noreply.github.com>
2021-09-17 10:10:28 +0100

    fix(week_one): PGCD

--- src/week_3/PGDC.java
+++ src/week_3/PGDC.java
@@ -3,22 +3,10 @@ package week_3;
 import java.util.Scanner;
 
 public class PGDC {
-	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
+	public static int getPgcd(int a, int b) {
+		int res = Math.max(a, b);
+		if (a != 0 || b != 0) {
 
-		System.out.print("Entrez un nombre positif : ");
-		int nb1 = scanner.nextInt();
-		System.out.print("Entrez un nombre positif : ");
-		int nb2 = scanner.nextInt();
-		if (nb1 == 0 && nb2 == 0) {
-			System.out.println("Le plus grand diviseur commun de " + nb1 + " et " + nb2 + " est " + nb1);
-		} else if (nb1 == 0) {
-			System.out.println("Le plus grand diviseur commun de " + nb1 + " et " + nb2 + " est " + nb2);
-		} else if (nb2 == 0) {
-			System.out.println("Le plus grand diviseur commun de " + nb1 + " et " + nb2 + " est " + nb1);
-		} else {
-			int a = nb1;
-			int b = nb2;
 			while (a != b) {
 				if (a > b) {
 					a = a - b;
@@ -26,8 +14,26 @@ public class PGDC {
 					b = b - a;
 				}
 			}
-			System.out.println("Le plus grand diviseur commun de " + nb1 + " et " + nb2 + " est " + a);
-			scanner.close();
+			res = a;
+
 		}
+		return res;
+
+	}
+	
+	
+
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.print("Entrez un nombre positif : ");
+		int nb1 = scanner.nextInt();
+		System.out.print("Entrez un nombre positif : ");
+		int nb2 = scanner.nextInt();
+		scanner.close();
+
+		System.out.println("Le plus grand diviseur commun de " + nb1 + " et " + nb2 + " est " + getPgcd(nb1,nb2));
+
 	}
 }
